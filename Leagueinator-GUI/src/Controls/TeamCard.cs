982d82df33a6e6cd60ba18799bede5ee22544728
Ed Armstrong <ed@Desktop-Orchid.localdomain>
2025-07-04 10:22:17 -0400

    removed redundant methods from TeamCard

--- Leagueinator-GUI/src/Controls/TeamCard.cs
+++ Leagueinator-GUI/src/Controls/TeamCard.cs
@@ -2,7 +2,6 @@
 using Leagueinator.GUI.Utility.Extensions;
 using System.Windows;
 using System.Windows.Controls;
-using static Leagueinator.GUI.Controllers.DragDropDelegates;
 
 namespace Leagueinator.GUI.Controls {
     public class TeamCard : Border {
@@ -14,11 +13,9 @@ namespace Leagueinator.GUI.Controls {
 
         #region Properties
 
-        public BowlsPanel BowlsPanel => this.Ancestors<BowlsPanel>().First();
-
         public MatchCard MatchCard => this.Ancestors<MatchCard>().First();
 
-        public static readonly DependencyProperty TeamIndexProperty =
+        private static readonly DependencyProperty TeamIndexProperty =
             DependencyProperty.Register(nameof(TeamIndex), typeof(int), typeof(TeamCard));
 
         public int TeamIndex {
@@ -26,7 +23,7 @@ namespace Leagueinator.GUI.Controls {
             set => SetValue(TeamIndexProperty, value);
         }
 
-        public static readonly DependencyProperty BowlsProperty =
+        private static readonly DependencyProperty BowlsProperty =
             DependencyProperty.Register(nameof(Bowls), typeof(int), typeof(TeamCard));
 
         public int Bowls {
@@ -35,6 +32,8 @@ namespace Leagueinator.GUI.Controls {
         }
 
         #endregion
+
+        #region Handles
 
         private void HndLoaded(object sender, RoutedEventArgs e) {
             DragDropController controller = new DragDropController(this);
@@ -53,58 +52,11 @@ namespace Leagueinator.GUI.Controls {
                 });
         }
 
-        /// <summary>
-        /// Determines whether any <see cref="TextBox"/> within the current object
-        /// contains the specified name as its text.
-        /// </summary>
-        /// <param name="name">The name To search for within the text of <see cref="TextBox"/> controls.</param>
-        /// <returns>
-        /// <c>true</c> if any <see cref="TextBox"/> contains the specified name as its text;
-        /// otherwise, <c>false</c>.
-        /// </returns>
-        /// <remarks>
-        /// This method searches through all <see cref="TextBox"/> descendants of the current object
-        /// and returns <c>true</c> if a match is found.
-        /// </remarks>
-        public bool HasName(string name) {
-            return this.GetDescendantsOfType<TextBox>()
-                       .Where(textBox => textBox.HasTag("PlayerName"))
-                       .Where(textBox => textBox.Text == name)
-                       .Any();
-        }
+        #endregion
 
         public void SetName(string name, int position) {
             TextBox[] boxes = this.FindByTag("PlayerName").OfType<TextBox>().ToArray();
             boxes[position].Text = name;
-        }
-
-        /// <summary>
-        /// Removes the specified name from all <see cref="TextBox"/> controls that are descendants
-        /// of the current object. If a <see cref="TextBox"/> contains the specified name, 
-        /// its text is cleared.
-        /// </summary>
-        /// <param name="name">The name To remove from the text boxes.</param>
-        public void RemoveName(string name) {
-            this.GetDescendantsOfType<TextBox>()
-                .Where(textBox => textBox.HasTag("PlayerName"))
-                .Where(textBox => textBox.Text == name)
-                .ToList()
-                .ForEach(textBox => {
-                    textBox.Clear();
-                });
-        }
-
-        /// <summary>
-        /// Clears the text from all <see cref="TextBox"/> controls that are descendants
-        /// of the current object. 
-        /// </summary>
-        public void ClearNames() {
-            this.GetDescendantsOfType<TextBox>()
-                .Where(textBox => textBox.HasTag("PlayerName"))
-                .ToList()
-                .ForEach(textBox => {
-                    textBox.Clear();
-                });
         }
     }
 }
