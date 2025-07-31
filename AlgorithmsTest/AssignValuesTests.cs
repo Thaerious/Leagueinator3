ed5def942e34f5766fd40137ae776435a15c6a55
Ed Armstrong <ed@Desktop-Orchid.localdomain>
2025-07-31 12:55:56 -0400

    Added test for AssignValues

---
+++ AlgorithmsTest/AssignValuesTests.cs
@@ -0,0 +1,58 @@
+﻿namespace Algorithms.Tests {
+    [TestClass]
+    public class AssignValuesTests {
+
+        [TestMethod]
+        public void TestBasicAssignment() {
+            var input = new Dictionary<string, List<int>> {
+                ["A"] = [1],
+                ["B"] = [2]
+            };
+
+            var assigner = new AssignValues<string, int>(input);
+            var result = assigner.Run();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result["A"]);
+            Assert.AreEqual(2, result["B"]);
+        }
+
+        [TestMethod]
+        public void TestNoValidAssignment() {
+            var input = new Dictionary<string, List<int>> {
+                ["A"] = [1],
+                ["B"] = [1]
+            };
+
+            var assigner = new AssignValues<string, int>(input);
+            var result = assigner.Run();
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void TestEmptyInput() {
+            var input = new Dictionary<string, List<int>>();
+            var assigner = new AssignValues<string, int>(input);
+            var result = assigner.Run();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void TestMultipleValidAssignments() {
+            var input = new Dictionary<string, List<int>> {
+                ["A"] = [1, 2],
+                ["B"] = [2, 1]
+            };
+
+            var assigner = new AssignValues<string, int>(input);
+            var result = assigner.Run();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Count);
+            Assert.AreNotEqual(result["A"], result["B"]);
+        }
+    }
+}
