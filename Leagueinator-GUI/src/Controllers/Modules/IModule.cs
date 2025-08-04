49d8f336bfa5fb87f5059683f8ee26286d046331
Ed Armstrong <ed@Desktop-Orchid.localdomain>
2025-08-04 14:47:43 -0400

    Readded IModule

---
+++ Leagueinator-GUI/src/Controllers/Modules/IModule.cs
@@ -0,0 +1,10 @@
+﻿using Leagueinator.GUI.Model;
+using System.Windows;
+
+namespace Leagueinator.GUI.Controllers.Modules {
+    public interface IModule {
+        public void LoadModule(Window targetWindow, MainController mainController);
+
+        public void UnloadModule();
+    }
+}
