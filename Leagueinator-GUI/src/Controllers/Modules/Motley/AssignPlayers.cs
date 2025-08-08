d8d9629a2f6f0f1d9635cff93cff77716af02890
Ed Armstrong <ed@Desktop-Orchid.localdomain>
2025-08-08 13:06:58 -0400

    motley match making working for pairs

--- Leagueinator-GUI/src/Controllers/Modules/Motley/AssignPlayers.cs
+++ Leagueinator-GUI/src/Controllers/Modules/Motley/AssignPlayers.cs
@@ -19,17 +19,29 @@ namespace Leagueinator.GUI.Controllers.Modules.Motley {
         }
 
         public RoundData Run() {
+            // The new round that will be returned
             RoundData roundData = new(this.EventData);
-            this.ELO = Motley.ELO.CalculateELO(this.LeagueData);
-            List<string> names = [.. RoundData.Records().Select(r => r.Name)];
-            List<string> setAside = [];
 
-            // Keep only players from the source round (ELO has all players from the league).
+            // Create a list of thisRoundPlayers from the ELO rankings
+            this.ELO = Motley.ELO.CalculateELO(this.LeagueData);
+            List<string> thisRoundPlayers = [.. RoundData.Records().Select(r => r.Name)];
+
+            // Keep only players from the source round (ELO provides players from the whole league).
             foreach (var name in this.ELO.Keys.ToList()) {
-                if (!names.Contains(name)) {
+                if (!thisRoundPlayers.Contains(name)) {
                     this.ELO.Remove(name);
                 }
             }
+
+            // Add new players from this round
+            foreach (var player in thisRoundPlayers) {
+                if (!this.ELO.ContainsKey(player)) {
+                    this.ELO[player] = 2000;
+                }
+            }
+
+            // Extra players
+            List<string> setAside = [];
 
             // Make the ELO list even
             if (ELO.Count % 2 != 0) {
@@ -43,19 +55,58 @@ namespace Leagueinator.GUI.Controllers.Modules.Motley {
                 });
             }
 
-            // Make the ELO divisable by 4
-            if (ELO.Count % 4 != 0) {
-                setAside = this.SetAside(2);
-                foreach (string name in setAside) this.ELO.Remove(name);
-                this.AssignTeams(roundData);
-            }
-            else {
-                this.AssignTeams(roundData);
-            }
-
+            this.AssignTeams(roundData);
             var matchesAssigned = this.AssignMatches(roundData);
             return matchesAssigned;
             
+        }
+
+        private RoundData AssignTeams(RoundData roundData) {
+            // Build a graph, players are nodes, connected to players they can play with.
+            // The edges are weighted with the elo differece between the two players.
+            Graph<string> graph = new();
+            GreedyAugmenting<string> pairingAlgorithm = new(graph);
+
+            foreach (var forPlayer in this.ELO) {
+                List<string> previousPartners = this.EventData.AllTeams()
+                                                    .Where(t => t.Names.Contains(forPlayer.Key))
+                                                    .SelectMany(t => t.Names)
+                                                    .Where(n => n != forPlayer.Key)
+                                                    .ToList();
+
+                // Add edges to graph between all players haven't been partners this round
+                foreach (var otherPlayer in this.ELO) {
+                    if (forPlayer.Key == otherPlayer.Key) continue;
+                    if (!previousPartners.Contains(otherPlayer.Key)) {
+                        int diff = Math.Abs(forPlayer.Value - otherPlayer.Value);
+                        graph.AddEdge(forPlayer.Key, otherPlayer.Key, diff * diff);
+                    }
+                }
+            }
+
+            Debug.WriteLine(graph.ToCSV());
+
+            // Run the pairing algorithm to pair players up.
+            Solution<string>? best = pairingAlgorithm.Run(50, 50);
+            if (best == null) throw new Exception("No pairings generated");
+
+            Debug.WriteLine($"Fitness = {best.Fitness}");
+
+            // Add matches to the round for each pairing (1 match for 2 pairs).
+            int team = 0; 
+            for (int i = 0; i < best!.Count; i++) {
+                MatchData match = new(roundData) {
+                    MatchFormat = MatchFormat.VS2,
+                    Ends = this.EventData.DefaultEnds
+                };
+
+                Debug.WriteLine($"{best[i].Item1}, {best[i].Item2}, {graph.GetEdge(best[i].Item1, best[i].Item2).Weight}");
+                match.Teams[team].CopyFrom([best[i].Item1, best[i].Item2]);
+                roundData.AddMatch(match);
+                team = team == 0 ? 1 : 0; // team flips between 0 and 1
+            }
+
+            return roundData;
         }
 
         private RoundData AssignMatches(RoundData roundData) {
@@ -91,46 +142,6 @@ namespace Leagueinator.GUI.Controllers.Modules.Motley {
             }
 
             return newRound;
-        }
-
-        private RoundData AssignTeams(RoundData roundData) {
-            Graph<string> graph = new();
-            GreedyAugmenting<string> pairingAlgorithm = new(graph);
-
-            foreach (var kv1 in this.ELO) {
-                string forPlayer = kv1.Key;
-
-                List<string> previousPartners = this.EventData.AllTeams()
-                                                    .Where(t => t.Names.Contains(forPlayer))
-                                                    .SelectMany(t => t.Names)
-                                                    .Where(n => n != forPlayer)
-                                                    .ToList();
-
-                // Add edges to graph between all players that weren't partners
-                foreach (var otherPlayer in this.ELO) {
-                    if (forPlayer == otherPlayer.Key) continue;
-                    if (!previousPartners.Contains(otherPlayer.Key)) {
-                        graph.AddEdge(forPlayer, otherPlayer.Key, Math.Abs(kv1.Value - otherPlayer.Value));
-                    }
-                }
-            }
-
-            Solution<string>? best = pairingAlgorithm.Run(20, 20);
-
-            if (best == null) throw new Exception("No pairings generated");
-
-            for (int i = 0; i < best!.Count; i += 2) {
-                MatchData match = new(roundData) {
-                    MatchFormat = MatchFormat.VS2,
-                    Ends = this.EventData.DefaultEnds
-                };
-
-                match.Teams[0].CopyFrom([best[i].Item1, best[i].Item2]);
-                match.Teams[1].CopyFrom([best[i + 1].Item1, best[i + 1].Item2]);
-                roundData.AddMatch(match);
-            }
-
-            return roundData;
         }
 
         /// <summary>
